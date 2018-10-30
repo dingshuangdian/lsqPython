48d6e73ecb8e51ea4e6629591d1ff2021ca2a113
dingshuangdian <286502698@qq.com>
2018-10-30 23:17:02 +0800

    updata

---
+++ MySql/day2/MySql连表操作.py
@@ -0,0 +1,7 @@
+# Create by dingshuangdian
+
+"""
+连表操作
+
+ select *from userinfo5,department5 where userinfo5.part_id=department5.id;
+"""
--- day36/进程.py
+++ day36/进程.py
@@ -4,20 +4,23 @@
 # 多个进程可以同时执行  并发
 # 我们现在写的一个py文件就自己一个进程  同步执行代码
 # 并发效果:
-    # 在我们自己的一个py文件里 启动多个进程
-    # 多个进程之间 - 操作系统
+# 在我们自己的一个py文件里 启动多个进程
+# 多个进程之间 - 操作系统
 # 如何在自己的py文件里 启动一个进程
 import os
 import time
 from multiprocessing import Process
+
+
 def func():
     print('heiheihei')
     time.sleep(1)
     print(os.getpid())
     print('hahaha')
 
+
 if __name__ == '__main__':
-    print('**',os.getpid())
+    print('**', os.getpid())
     p = Process(target=func)
-    p.start()    #启动一个进程
-    print('**',os.getpid())
+    p.start()  # 启动一个进程
+    print('**', os.getpid())
